2f7a565b7a5802f470925cff4e3f9650f114b338
Abhinav_PP <abhinavabhi1441@gmail.com>
2024-01-10 20:59:26 +0530

    School Website Design Created

---
+++ JS/app.js
@@ -0,0 +1,10 @@
+// toggle menu
+var navLinks = document.getElementById("NavLinks");
+
+function showMenu() {
+  navLinks.style.right = "0";
+}
+function hideMenu() {
+  navLinks.style.right = "-200px";
+}
+// ---------
